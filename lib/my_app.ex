106256ab904a4a10baa5ffd3ebcc1bbd6e63286c
Kamil Skowron <emanuel1989@gmail.com>
2021-08-06 15:00:44 +0100

    Working dp test

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,16 +1,17 @@
 defmodule MyApp do
   use GenServer
 
-  def start_link(acc) do
-    GenServer.start_link(__MODULE__, acc)
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args)
   end
 
-  def init(acc) do
-    {:ok, acc}
+  def init({data, context}) do
+    storage_write = Map.get(context, :storage_write, &File.write!/3)
+    {:ok, {data, %{storage_write: storage_write}}}
   end
 
-  def handle_call(:inc, _from, acc) do
-    :ok = File.write!("cache.txt", to_string(acc + 1))
-    {:reply, acc + 1, acc + 1}
+  def handle_call(:inc, _from, {acc, context}) do
+    :ok = context.storage_write.("cache.txt", to_string(acc + 1))
+    {:reply, acc + 1, {acc + 1, context}}
   end
 end
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -1,8 +1,11 @@
 defmodule MyAppTest do
   use ExUnit.Case
-  doctest MyApp
 
-  test "greets the world" do
-    assert MyApp.hello() == :world
+  test "Increments the counter" do
+    {:ok, pid} = MyApp.start_link({0, %{
+      storage_write: fn (_path, "1") -> :ok end
+    }})
+
+    assert 1 == GenServer.call(pid, :inc)
   end
 end
