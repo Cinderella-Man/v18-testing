0f8e44c106af6b9db540614e234dfbf470286034
Kamil Skowron <emanuel1989@gmail.com>
2021-08-06 23:26:25 +0100

    Function test

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,17 +1,21 @@
 defmodule MyApp do
   use GenServer
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+  def start_link(acc) do
+    GenServer.start_link(__MODULE__, acc)
   end
 
-  def init({data, context}) do
-    storage_write = Map.get(context, :storage_write, &File.write!/3)
-    {:ok, {data, %{storage_write: storage_write}}}
+  def init(acc) do
+    {:ok, acc}
   end
 
-  def handle_call(:inc, _from, {acc, context}) do
-    :ok = context.storage_write.("cache.txt", to_string(acc + 1))
-    {:reply, acc + 1, {acc + 1, context}}
+  def handle_call(:inc, _from, acc) do
+    acc = increment_accumulator(acc)
+    {:reply, acc, acc}
+  end
+
+  def increment_accumulator(acc, write_fun \\ &File.write!/3) do
+    :ok = write_fun.("cache.txt", to_string(acc + 1))
+    acc + 1
   end
 end
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -2,10 +2,9 @@ defmodule MyAppTest do
   use ExUnit.Case
 
   test "Increments the counter" do
-    {:ok, pid} = MyApp.start_link({0, %{
-      storage_write: fn (_path, "1") -> :ok end
-    }})
-
-    assert 1 == GenServer.call(pid, :inc)
+    assert 1 == MyApp.increment_accumulator(
+      0,
+      fn (_path, "1") -> :ok end
+    )
   end
 end
