fbdc010fd3159680307cd9a1efe8f72c1ac172fa
Kamil Skowron <emanuel1989@gmail.com>
2021-08-09 13:55:14 +0100

    Interface function dependency injection

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -9,13 +9,12 @@ defmodule MyApp do
     {:ok, acc}
   end
 
-  def handle_call(:inc, _from, acc) do
-    acc = increment_accumulator(acc)
-    {:reply, acc, acc}
+  def handle_call({:inc, write_fun}, _from, acc) do
+    :ok = write_fun.("cache.txt", to_string(acc + 1))
+    {:reply, acc + 1, acc + 1}
   end
 
-  def increment_accumulator(acc, write_fun \\ &File.write!/3) do
-    :ok = write_fun.("cache.txt", to_string(acc + 1))
-    acc + 1
+  def increment_accumulator(pid, write_fun \\ &File.write!/3) do
+    GenServer.call(pid, {:inc, write_fun})
   end
 end
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -2,9 +2,12 @@ defmodule MyAppTest do
   use ExUnit.Case
 
   test "Increments the counter" do
-    assert 1 == MyApp.increment_accumulator(
-      0,
-      fn (_path, "1") -> :ok end
-    )
+    {:ok, pid} = MyApp.start_link(0)
+
+    assert 1 ==
+             MyApp.increment_accumulator(
+               pid,
+               fn _path, "1" -> :ok end
+             )
   end
 end
