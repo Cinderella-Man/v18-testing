ece904e6081eead37cb2c6952c2eb9d777e92c55
Kamil Skowron <emanuel1989@gmail.com>
2021-08-27 17:48:26 +0100

    mix format

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -2,9 +2,13 @@ defmodule MyAppTest do
   use ExUnit.Case
 
   test "Increments the counter" do
-    {:ok, pid} = MyApp.start_link({0, %{
-      storage_write: fn (_path, "1") -> :ok end
-    }})
+    {:ok, pid} =
+      MyApp.start_link(
+        {0,
+         %{
+           storage_write: fn _path, "1" -> :ok end
+         }}
+      )
 
     assert 1 == GenServer.call(pid, :inc)
   end
